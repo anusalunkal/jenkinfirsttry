75ad9df69ebb74fccc8844aae00eba50a084514d
resh226 <reshmasajeevpulickal@gmail.com>
2020-01-22 19:13:46 +0530

    third commit

--- one.js
+++ one.js
@@ -1,3 +1,11 @@
-console.log("checking");
-console.log("integrating with jenkins");
-console.log("updation");
+//console.log("checking");
+//console.log("integrating with jenkins");
+describe('Jenkin trial', function() {
+    it('openinggoogle', function() {
+        browser.waitForAngularEnabled(false);
+        browser.get('https://www.google.com/');
+      browser.sleep('10000');
+      console.log("checking");
+     });
+  });
+
